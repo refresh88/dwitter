e3e8f72ad304fdb59b079cd7f915de209add8508
refresh88 <ouok88@gmail.com>
2023-01-05 22:09:09 +0900

    validation 추가
    post, put에 body('text') validation 추가

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,8 +1,18 @@
 import express from 'express';
 import 'express-async-errors';
+import { body } from 'express-validator';
 import * as tweetController from '../controller/tweet.js';
+import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
+
+const validateTweet = [
+  body('text')
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage('text shuld be at least 3 characters'),
+  validate,
+];
 
 //GET /tweets
 //GET /tweets?username=:username
@@ -12,10 +22,10 @@ router.get('/', tweetController.getTweets);
 router.get('/:id', tweetController.getTweet);
 
 //POST /tweets
-router.post('/', tweetController.updateTweet);
+router.post('/', validateTweet, tweetController.updateTweet);
 
 //PUT /tweets/:
-router.put('/:id', tweetController.createTweet);
+router.put('/:id', validateTweet, tweetController.createTweet);
 
 //DELTE /tweets/:id
 router.delete('/:id', tweetController.deleteTweet);
